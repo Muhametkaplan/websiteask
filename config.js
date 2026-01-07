1dd967889d5785f8ed10d7120b70c3732bd0fdba
Muhametkaplan <muhammet.2713ka@gmail.com>
2026-01-08 01:53:21 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+// js/config.js
+window.APP_CONFIG = {
+    FIREBASE: {
+        apiKey: "AIzaSyAuryeEz2QmtfnbokrSvGWwPIWCMqXxti8",
+        authDomain: "ask-sitem.firebaseapp.com",
+        projectId: "ask-sitem",
+        storageBucket: "ask-sitem.firebasestorage.app",
+        messagingSenderId: "1093033362074",
+        appId: "1:1093033362074:web:ac33672c8221225e0a9523",
+        measurementId: "G-N0MY21Y05K"
+    },
+    GEMINI_KEY: "AIzaSyAu4yCFxNYqEea1twMocJy4U5xTHUQMDaU",
+    START_DATE: new Date(2025, 9, 15) // Tarihini buraya gir
+};
